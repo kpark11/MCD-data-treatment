c73b2d50d8009038636446f47ced112d9c3ea2da
Kiman Park <95437032+kpark11@users.noreply.github.com>
2021-12-03 13:09:07 -0500

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -14,7 +14,7 @@ import fnmatch
 
 ########### Setting up ###############
 
-file = r'D:\Raw 12K Data'
+file = r''
 os.chdir(file)
 cwd = os.getcwd()
 print(cwd)
@@ -49,16 +49,16 @@ for filename in list1:
     elif fnmatch.fnmatch(filename,'*MCD*'):
         data = pd.read_csv(filename,delimiter='\t',header=None)
         data_MCD['Wavelength'] = data[0]
-        #data_MCD['X_'+filename] = data[1]
-        #data_MCD['Y_'+filename] = data[2]
-        #data_MCD['XX_'+filename] = data[3]
-        #data_MCD['YYY_'+filename] = data[4]
+        data_MCD['X_'+filename] = data[1]
+        data_MCD['Y_'+filename] = data[2]
+        data_MCD['XX_'+filename] = data[3]
+        data_MCD['YYY_'+filename] = data[4]
         data_MCD['MCD_'+filename] = data[5]
         plt.plot(data[0],data[5])
-        #plt.xlabel('Wavelength (nm)')
-        #plt.ylabel('Raw MCD signal')
-        #plt.title(filename)
-        #plt.show()
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('Raw MCD signal')
+        plt.title(filename)
+        plt.show()
     else:
         pass
     
@@ -68,10 +68,11 @@ for filename in list1:
 plt.xlabel('Wavelength (nm)')
 plt.ylabel('Raw MCD signal')
 plt.title(filename)
-plt.axvline(x=457)
-plt.axvline(x=575)
-plt.axvline(x=700)
-plt.axvline(x=800)
+plt.axvline(x=556)
+plt.axvline(x=604)
+plt.axvline(x=663)
+plt.axvline(x=761)
+plt.axvline(x=845)
 plt.savefig("MCD_together.pdf")
 plt.show()
 
@@ -89,6 +90,7 @@ treated_MCD_15T = data_MCD['MCD_15T_up'] - data_MCD['MCD_n15T_up']
 treated_MCD_10T = data_MCD['MCD_10T_up'] - data_MCD['MCD_n10T_up']
 treated_MCD_7T = data_MCD['MCD_7T_up'] - data_MCD['MCD_n7T_up']
 treated_MCD_5T = data_MCD['MCD_5T_up'] - data_MCD['MCD_n5T_up']
+treated_MCD_2T = data_MCD['MCD_2T_up'] - data_MCD['MCD_n2T_up']
 treated_MCD_0p5T = data_MCD['MCD_0p5T_up'] - data_MCD['MCD_n0p5T_up']
 
 
@@ -98,6 +100,7 @@ treated_MCD_n15T = -(data_MCD['MCD_15T_down'] - data_MCD['MCD_n15T_down'])
 treated_MCD_n10T = -(data_MCD['MCD_10T_down'] - data_MCD['MCD_n10T_down'])
 treated_MCD_n7T = -(data_MCD['MCD_7T_down'] - data_MCD['MCD_n7T_down'])
 treated_MCD_n5T = -(data_MCD['MCD_5T_down'] - data_MCD['MCD_n5T_down'])
+treated_MCD_n2T = -(data_MCD['MCD_2T_down'] - data_MCD['MCD_n2T_down'])
 treated_MCD_n0p5T = -(data_MCD['MCD_0p5T_down'] - data_MCD['MCD_n0p5T_down'])
 
 
@@ -120,13 +123,18 @@ plt.plot(data_MCD['Wavelength'],treated_MCD_n25T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_20T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_15T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_10T)
+plt.plot(data_MCD['Wavelength'],treated_MCD_7T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_5T)
-plt.plot(data_MCD['Wavelength'],treated_MCD_5T2nd)
+plt.plot(data_MCD['Wavelength'],treated_MCD_2T)
+plt.plot(data_MCD['Wavelength'],treated_MCD_0p5T)
 
-plt.plot(data_MCD['Wavelength'],treated_MCD_20T_2)
-plt.plot(data_MCD['Wavelength'],treated_MCD_15T_2)
-plt.plot(data_MCD['Wavelength'],treated_MCD_10T_2)
-plt.plot(data_MCD['Wavelength'],treated_MCD_5T_2)
+plt.plot(data_MCD['Wavelength'],treated_MCD_n20T)
+plt.plot(data_MCD['Wavelength'],treated_MCD_n15T)
+plt.plot(data_MCD['Wavelength'],treated_MCD_n10T)
+plt.plot(data_MCD['Wavelength'],treated_MCD_n7T)
+plt.plot(data_MCD['Wavelength'],treated_MCD_n5T)
+plt.plot(data_MCD['Wavelength'],treated_MCD_n2T)
+plt.plot(data_MCD['Wavelength'],treated_MCD_n0p5T)
 
 plt.plot(data_MCD['Wavelength'],treated_MCD_0T)
 
