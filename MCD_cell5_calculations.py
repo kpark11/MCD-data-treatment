d0f2da0cd1ee6b47d65dc91613bd4a830c0290c2
Kiman Park <95437032+kpark11@users.noreply.github.com>
2021-12-03 22:46:18 -0500

    Update MCD_cell5_calculations.py

--- MCD_cell5_calculations.py
+++ MCD_cell5_calculations.py
@@ -14,7 +14,7 @@ import fnmatch
 
 ########### Setting up ###############
 
-file = r''
+file = r'C:\Users\brian\OneDrive - University of Tennessee\Desktop\Research\CrPS4\Maglab 12_3_2021\CrPS4'
 os.chdir(file)
 cwd = os.getcwd()
 print(cwd)
@@ -63,48 +63,36 @@ for filename in list1:
         pass
     
     
-############### Plots and treatment ############
 
-plt.xlabel('Wavelength (nm)')
-plt.ylabel('Raw MCD signal')
-plt.title(filename)
-plt.axvline(x=556)
-plt.axvline(x=604)
-plt.axvline(x=663)
-plt.axvline(x=761)
-plt.axvline(x=845)
-plt.savefig("MCD_together.pdf")
-plt.show()
+
+
+
 
 ########################
 
 
 
 
-treated_MCD_25T = data_MCD['MCD_25T'] - data_MCD['MCD_n25T']
-treated_MCD_n25T = data_MCD['MCD_n25T'] - data_MCD['MCD_25T']
+treated_MCD_25T = data_MCD['25T_up_MCD'] - data_MCD['n25T_down_MCD']
+treated_MCD_n25T = data_MCD['n25T_down_MCD'] - data_MCD['25T_up_MCD']
 
 
-treated_MCD_20T = data_MCD['MCD_20T_up'] - data_MCD['MCD_n20T_up']
-treated_MCD_15T = data_MCD['MCD_15T_up'] - data_MCD['MCD_n15T_up']
-treated_MCD_10T = data_MCD['MCD_10T_up'] - data_MCD['MCD_n10T_up']
-treated_MCD_7T = data_MCD['MCD_7T_up'] - data_MCD['MCD_n7T_up']
-treated_MCD_5T = data_MCD['MCD_5T_up'] - data_MCD['MCD_n5T_up']
-treated_MCD_2T = data_MCD['MCD_2T_up'] - data_MCD['MCD_n2T_up']
-treated_MCD_0p5T = data_MCD['MCD_0p5T_up'] - data_MCD['MCD_n0p5T_up']
+treated_MCD_20T = data_MCD['20T_up_MCD'] - data_MCD['n20T_down_MCD']
+treated_MCD_15T = data_MCD['15T_up_MCD'] - data_MCD['n15T_down_MCD']
+treated_MCD_10T = data_MCD['10T_up_MCD'] - data_MCD['n10T_down_MCD']
+treated_MCD_5T = data_MCD['5T_up_MCD'] - data_MCD['n5T_down_MCD']
 
 
 
-treated_MCD_n20T = -(data_MCD['MCD_20T_down'] - data_MCD['MCD_n20T_down'])
-treated_MCD_n15T = -(data_MCD['MCD_15T_down'] - data_MCD['MCD_n15T_down'])
-treated_MCD_n10T = -(data_MCD['MCD_10T_down'] - data_MCD['MCD_n10T_down'])
-treated_MCD_n7T = -(data_MCD['MCD_7T_down'] - data_MCD['MCD_n7T_down'])
-treated_MCD_n5T = -(data_MCD['MCD_5T_down'] - data_MCD['MCD_n5T_down'])
-treated_MCD_n2T = -(data_MCD['MCD_2T_down'] - data_MCD['MCD_n2T_down'])
-treated_MCD_n0p5T = -(data_MCD['MCD_0p5T_down'] - data_MCD['MCD_n0p5T_down'])
+treated_MCD_n20T = -(data_MCD['20T_down_MCD'] - data_MCD['n20T_up_MCD'])
+treated_MCD_n15T = -(data_MCD['15T_down_MCD'] - data_MCD['n15T_up_MCD'])
+treated_MCD_n10T = -(data_MCD['10T_down_MCD'] - data_MCD['n10T_up_MCD'])
+treated_MCD_n5T = -(data_MCD['5T_down_MCD'] - data_MCD['n5T_up_MCD'])
 
 
-treated_MCD_0T = data_MCD['MCD_0T_down'] - data_MCD['MCD_0T_up']
+treated_MCD_0T = data_MCD['0T_down_MCD'] - data_MCD['n0T_up_MCD']
+treated_MCD_n0T = data_MCD['n0T_up_MCD'] - data_MCD['0T_down_MCD']
+
 
 
 
@@ -123,26 +111,26 @@ plt.plot(data_MCD['Wavelength'],treated_MCD_n25T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_20T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_15T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_10T)
-plt.plot(data_MCD['Wavelength'],treated_MCD_7T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_5T)
-plt.plot(data_MCD['Wavelength'],treated_MCD_2T)
-plt.plot(data_MCD['Wavelength'],treated_MCD_0p5T)
 
 plt.plot(data_MCD['Wavelength'],treated_MCD_n20T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_n15T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_n10T)
-plt.plot(data_MCD['Wavelength'],treated_MCD_n7T)
 plt.plot(data_MCD['Wavelength'],treated_MCD_n5T)
-plt.plot(data_MCD['Wavelength'],treated_MCD_n2T)
-plt.plot(data_MCD['Wavelength'],treated_MCD_n0p5T)
 
 plt.plot(data_MCD['Wavelength'],treated_MCD_0T)
+plt.plot(data_MCD['Wavelength'],treated_MCD_n0T)
+
 
 plt.xlabel('Wavelength (nm)')
-plt.ylabel('Raw MCD signal')
-plt.title('treated MCD')
+plt.ylabel('MCD signal')
+plt.title(filename)
+plt.axvline(x=556)
+plt.axvline(x=604)
+plt.axvline(x=663)
+plt.axvline(x=761)
+plt.axvline(x=845)
 plt.savefig("treated_MCD_together.pdf")
-
 plt.show()
 
 
@@ -150,10 +138,8 @@ plt.show()
 
 
 
-###############################
 
-
-
+############### Plots and treatment ############
 
 
 
@@ -163,23 +149,15 @@ data_MCD['treated_MCD_n25T']= treated_MCD_n25T
 data_MCD['treated_MCD_20T']= treated_MCD_20T
 data_MCD['treated_MCD_15T']= treated_MCD_15T
 data_MCD['treated_MCD_10T']= treated_MCD_10T
-data_MCD['treated_MCD_7T']= treated_MCD_7T
 data_MCD['treated_MCD_5T']= treated_MCD_5T
-data_MCD['treated_MCD_2T']= treated_MCD_2T
-data_MCD['treated_MCD_0p5T']= treated_MCD_0p5T
 
 data_MCD['treated_MCD_n20T']= treated_MCD_n20T
 data_MCD['treated_MCD_n15T']= treated_MCD_n15T
 data_MCD['treated_MCD_n10T']= treated_MCD_n10T
-data_MCD['treated_MCD_n7T']= treated_MCD_n7T
 data_MCD['treated_MCD_n5T']= treated_MCD_n5T
-data_MCD['treated_MCD_n2T']= treated_MCD_n2T
-data_MCD['treated_MCD_n0p5T']= treated_MCD_n0p5T
 
 data_MCD['treated_MCD_0T']= treated_MCD_0T
-
-
-
+data_MCD['treated_MCD_n0T']= treated_MCD_n0T
 
 
 
